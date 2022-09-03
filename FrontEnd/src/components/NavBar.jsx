18e287408e64185b40cdc4230646661a0ce6f783
masete <nicholasmasete72@gmail.com>
2022-09-03 13:04:14 +0300

    feat: working on the navbar

--- FrontEnd/src/components/NavBar.jsx
+++ FrontEnd/src/components/NavBar.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between items-center w-2/3 right-0 p-4 h-20 fixed text-black border border-gray-500'>
       <div>
-          <p>PhotoView</p>
+          <h1 className='text-6xl font-signature ml-4'>PhotoView</h1>
+      </div>
+      <div className=''>
+        <button>Create</button>
+      </div>
+      <div>
+        <p>profile</p>
       </div>
     </div>
   )
---
+++ FrontEnd/src/components/SideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <>
+    <div>
+        <p>SideBar</p>
+        <div className='flex flex-col inline'>
+            <ul>
+                <li>Photographer</li>
+                <li>User</li>
+                <li>Admin</li>
+            </ul>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default SideBar
