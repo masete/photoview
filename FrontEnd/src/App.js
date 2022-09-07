abcc7e8ec42032cbbce78d4a558057803d674727
masete <nicholasmasete72@gmail.com>
2022-09-07 23:24:41 +0300

    fix: refactore to use react router do for navigation

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import NavBar from './components/NavBar';
 import SideBar from './components/SideBar';
 import Dashboard from './pages/Dashboard';
+import Timeline from './pages/Timeline';
 
 function App() {
   
   return (
     <>
-    <div>
-      <NavBar />
-      <SideBar />
-      <Dashboard />
-    </div>
-    
+    <Router>
+      <Route path='/timeline' element={<Timeline/>} />
+    </Router>
     </>
     
   );
